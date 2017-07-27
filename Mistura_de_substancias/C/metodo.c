54ef7f04940c80405a543e2dc4553c9582136e77
George <gvictor00@gmail.com>
2017-07-26 21:10:39 -0300

    Correção C e adição Python

--- Mistura_de_substancias/C/metodo.c
+++ Mistura_de_substancias/C/metodo.c
@@ -2,54 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*
-    Ti = Taxa de entrada de Sal
-    Tf = Taxa de saída
-    ti = Tempo inicial (0)
-    t = Tempo final
-    C = Constante de decaimento
-    h = O tamanho das subdiviões de X
-    n = O número de subdivisões de X
-
-    Ti = 80 ºC
-    Tf = 0 ºC
-    ti = 0 minutos
-    k = 0.1386
-
-    Alguns exemplos:
-    t = 30 minutos => T = 1.25 ºC
-    t = 5 minutos => T = 40 ºC
-    t = 10 minutos => T = 20 ºC
-*/
-
-/*
-    Q0 = Quantidade Inicial
-    Q(t) = Quantidade em função do tempo
-    lambda = Constante de decaimento
-    Tf = Tempo final
-    Ti = Tempo inicial
-
-*/
-double Ti = 0;  // Tempo inicial
-double Tf = 0;  // Tempo final
-double C = 0;   // Contante
-double t = 0;   // Tempo
+double Ti = 50;  // Tempo inicial
+double Tf = 0.01;  // Tempo final
+double C = -4900;   // Contante
+double t = 1;   // Tempo
 double ti = 0;  // Tempo inicial
+int n = 0;
 
 /// y(t) = T0/Tf + exp(-Tf*t)*C
 double solucao(){
     return (Ti/Tf + C*exp(-Tf*t));
 }
-/// Y'(t) = T0 - Tf*Y - ("Lei de Equilibrio")
-/// Y'(t) = Taxa de entrada de Sal - Taxa de saída de sal
+/// Y'(t) = T0 - Tf * Y - ("Lei de Equilibrio")
+/// Y'(t) = Taxa de entrada de Sal - Taxa de saída de sal * Quantidade presente na mistura
 
+double y_inicial(){
+    return (Ti/Tf + C*exp(-Tf*ti));
+}
 double f (double X, double Y)
 {
-    return Ti - Tf*Y;
+    return Ti + (-Tf*Y);
 }
 
 double metodo_euler ( int n ) {
-    double Y = Ti;
+    double Y = 100;
     double X = ti;
     double h = (t-ti)/n;
     int i;
@@ -63,7 +39,7 @@ double metodo_euler ( int n ) {
 }
 
 double metodo_euler_modificado ( int n ) {
-    double Y_atual = Ti;
+    double Y_atual = 100;
     double X_atual = ti;
     double Y_previsao;
     double X_proximo;
@@ -81,7 +57,7 @@ double metodo_euler_modificado ( int n ) {
 }
 
 double metodo_runge_kutta ( int n ) {
-    double Y = Ti;
+    double Y = 100;
     double X = ti;
     double h = (t-ti)/n;
     double k1, k2, k3, k4;
@@ -100,22 +76,62 @@ double metodo_runge_kutta ( int n ) {
     return Y;
 }
 
+double metodo_passos_multiplos()        //Adams-Bashforth-Moulton 4a Ordem
+{
+    //Variaveis auxiliares
+    int j = 0;
+    double K1 = 0, K2 = 0, K3 = 0, K4 = 0;
+
+    //Tamanho do passo
+    double h = (t-ti)/n;
+
+    //Vetor de soluções
+    double x[n+1], y[n+1]; 
+
+    // Etapa para cálculo Preditor-Corretor
+    double yp, yc;
+
+    for(j=0;j<n+1;j++)
+    {
+        x[j]=0;
+        y[j]=0;
+    }
+    
+    //Condição Inicial
+    x[0] = ti;
+    y[0] = y_inicial();
+
+    // Calcula os 3 proximos valores (x,y)
+    for(j=0;j<3;j++)
+    {
+        // Runge-Kutta 4a ordem
+        //===========================
+        K1 = f(x[j], y[j]);
+        K2 = f(x[j]+h/2, y[j]+(h/2)*K1);
+        K3 = f(x[j]+h/2, y[j]+(h/2)*K2);
+        K4 = f(x[j]+h, y[j]+h*K3);
+        x[j+1] = x[j] + h;
+        y[j+1] = y[j] + (h/6)*(K1 + 2*K2 + 2*K3 + K4);
+        //========================= 
+    }
+
+    for(j=3;j<n;j++)
+    {
+        x[j+1] = x[j] + h;
+        // Preditor
+        yp = y[j] + (h/24)*(55*f(x[j],y[j]) - 59*f(x[j-1],y[j-1]) + 37*f(x[j-2],y[j-2]) - 9*f(x[j-3],y[j-3]));
+        // Corretor
+        yc = y[j] + (h/24)*(9*f(x[j+1],yp) + 19*f(x[j],y[j]) - 5*f(x[j-1],y[j-1]) + f(x[j-2],y[j-2]));
+        y[j+1] = y[j] + (h/24)*(9*f(x[j+1],yc) + 19*f(x[j],y[j]) - 5*f(x[j-1],y[j-1]) + f(x[j-2],y[j-2]));
+    }
+
+    return y[n];
+}
 int main()
 {
-    int n;
-    double erro, resposta_solucao, resposta_euler;
+    double erro, resposta_solucao, resposta_euler, resposta_passos_mult;
     double resposta_euler_modificado;
     double resposta_runge_kutta;
-
-    printf("Digite as condicoes do problema\n");
-    printf("Taxa de entrada de sal (T0): ");
-    scanf("%lf", &Ti);
-    printf("Taxa de saida de sal (TF): ");
-    scanf("%lf", &Tf);
-    printf("Tempo: ");
-    scanf("%lf", &t);
-    printf("A constante de decaimento (0,1386): ");
-    scanf("%lf", &C);
 
     do {
         printf("---------------------------------------------\n");
@@ -128,7 +144,7 @@ int main()
             resposta_euler = metodo_euler(n);
             printf("Metodo de Euler: %.5lf\n", resposta_euler);
             erro = fabs(resposta_solucao - resposta_euler);
-            printf("O erro: %lf\n\n", erro);
+            printf("O erro: %.5lf\n\n", erro);
 
             resposta_euler_modificado = metodo_euler_modificado(n);
             printf("Metodo modificado de Euler: %.5lf\n", resposta_euler_modificado);
@@ -138,6 +154,11 @@ int main()
             resposta_runge_kutta = metodo_runge_kutta(n);
             printf("Metodo de Runge Kutta: %.5lf\n", resposta_runge_kutta);
             erro = fabs(resposta_solucao - resposta_runge_kutta);
+            printf("O erro: %.5lf\n\n", erro);
+
+            resposta_passos_mult = metodo_passos_multiplos(n);
+            printf("Metodo de Passos Multiplos: %.5lf\n", resposta_passos_mult);
+            erro = fabs(resposta_solucao - resposta_passos_mult);
             printf("O erro: %.5lf\n", erro);
         }
     } while ( n > 0 );
--- Mistura_de_substancias/README.md
+++ Mistura_de_substancias/README.md
@@ -1,13 +1,13 @@
 # Métodos numéricos
 
 ## O problema: Mistura de substâncias
+Problema descrito como `Exemplo 1 da página 29, capítulo 2 do livro` **Equações Diferenciais** [Boyce, Diprima]
 Problemas de mistura ocorrem com bastante frequência na indústria química. Por exemplo: determinar a concentração de uma certa substância, passado um tempo t, numa mistura; determinar a taxa de vazão de um líquido em um tanque industrial, etc.
 
 Nas atividades industriais, a maioria dos processos apresenta a necessidade de agitação e mistura em ao menos uma de suas etapas. No caso de um processo de mistura, supõe-se geralmente que os componentes da mistura são perfeitamente misturados para facilitar a modelagem. Quanto aos calculos, balanços de massa devem ser realizados para o componente que se deseja estudar, a fim de que se obtenha uma equação diferencial, e assim, seja possível conhecer o modelo que descreve o estado transiente do sistema (BOYCE; PRIMA,1994).
 
 O problema afirma o seguinte "a taxa de variação, no tempo, de uma substância e proporcional a diferença entre as taxas de entrada e saída da mesma". Este conceito e representado por uma equação diferencial ordinária: <br><br>
-<img src="https://latex.codecogs.com/gif.latex?\frac{dY}{dt}&space;=&space;T_0&space;-&space;Tf_y" title="\frac{dY}{dt} = T_0 - Tf_y"/>
-
+<img src="https://latex.codecogs.com/gif.latex?\frac{dY}{dt}&space;=&space;T_0&space;-&space;T_fY(t)" title="\frac{dY}{dt} = T_0 - T_fY(t)" />
 Por fim, após resolver a equação diferencial temos a seguinte equação:<br><br>
 <img src="https://latex.codecogs.com/gif.latex?y(t)=\frac{T_0}{T_f}&plus;Ce^{-Tft}" title="y(t)=\frac{T_0}{T_f}+Ce^{-Tft}" />
 
